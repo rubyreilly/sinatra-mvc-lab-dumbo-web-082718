938df89c1373a69c6d98356a7cd69bb9a552c292
github username <github email address>
2018-09-17 17:40:09 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    new_phrase = PigLatinizer.new
+    "#{new_phrase.piglatinize(params[:user_phrase].to_s)}"
+  end
+
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,39 @@
+class PigLatinizer
+
+  # attr_accessor :word
+
+  # def initialize
+  #   # @word = word
+  # end
+
+  @@vowels = [ "a", "e", "i", "o", "u"]
+
+  def piglatinize(string)
+
+    input_array = string.split(" ")
+    output_array = input_array.map do |word|
+      # "#{word[1..word.length]}" + "#{word[0]}" + "ay"
+      letter_array = word.split("")
+      if @@vowels.include?(letter_array[0].downcase)
+        word + "way"
+      else
+        new_word_array = letter_array
+        letter_array.each do |letter|
+          until @@vowels.include?(new_word_array[0].downcase)
+            new_word_array=new_word_array.rotate
+          end
+        end
+        new_word_array.join("") + "ay"
+      end
+
+    end
+
+    output_array.join(" ")
+
+  end
+
+
+
+
+
+end
